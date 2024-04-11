9f7b66354f39c674c3f01ff76e39cc2589424288
Ethan Duvall <eduvall23@turing.edu>
2024-04-11 11:27:13 -0600

    Working login with spotify page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,35 +1,42 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import React from "react";
+import Login from "../Login/Login";
+import "./App.css";
 
 function App() {
-  const clientID = "f8791f93dad4435cafbc64320e66c76f"
-  const clientSecret = "2f042acf3d6d4599ae16866c2531b98d"
+  const clientID = "f8791f93dad4435cafbc64320e66c76f";
+  const clientSecret = "2f042acf3d6d4599ae16866c2531b98d";
   const [token, setToken] = useState(null);
 
-  
   useEffect(() => {
     const requestOptions = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Authorization': 'Basic ' + btoa(`${clientID}:${clientSecret}`),
-        'Content-Type': 'application/x-www-form-urlencoded'
+        Authorization: "Basic " + btoa(`${clientID}:${clientSecret}`),
+        "Content-Type": "application/x-www-form-urlencoded",
       },
-      body: 'grant_type=client_credentials'
+      body: "grant_type=client_credentials",
     };
 
-    fetch('https://accounts.spotify.com/api/token', requestOptions)
-      .then(response => response.json())
-      .then(data => {
+    fetch("https://accounts.spotify.com/api/token", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
         if (data.access_token) {
           setToken(data.access_token);
         }
       })
-      .catch(error => console.error('Error:', error));
+      .catch((error) => console.error("Error:", error));
   }, []);
 
   return (
     <div>
       <p>Access Token: {token}</p>
+      <p>Client Id: {clientID}</p>
+      <Login clientId={clientID} />
+      <Routes>
+        <Route path="/dashboard" element={<p>YOU DID IT</p>} />
+      </Routes>
     </div>
   );
 }
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React from "react";
 
-const client_id = 'CLIENT_ID';
-const redirect_uri = 'http://localhost:8888/callback';
-
-function Login() {
-  const [state, setState] = useState(generateRandomString(16));
+function Login({ clientId }) {
+  const redirect_uri = 'http://localhost:3000/dashboard'; 
   const scope = 'user-read-private user-read-email';
-  const history = useHistory();
 
   const handleLogin = () => {
-    const url = `https://accounts.spotify.com/authorize?${new URLSearchParams({
-      response_type: 'code',
-      client_id: client_id,
-      scope: scope,
-      redirect_uri: redirect_uri,
-      state: state
-    }).toString()}`;
-    window.location.href = url;
+    const authorizationUrl = `https://accounts.spotify.com/authorize?response_type=token&client_id=${clientId}&scope=${scope}&redirect_uri=${redirect_uri}`;
+    
+    window.location.href = authorizationUrl;
   };
 
   return (
